9223e36a1b635f09bf647238a865a1d85660af0b
kiripon <painapoo@gmail.com>
2017-03-01 15:06:29 +0900

    fix

--- src/TypeCompletion.hs
+++ src/TypeCompletion.hs
@@ -1,33 +1,42 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE NondecreasingIndentation #-}
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP                      #-}
 
-module TypeCompletion where
+{-# LANGUAGE NondecreasingIndentation #-}
+{-# LANGUAGE ScopedTypeVariables      #-}
+
+module TypeCompletion
+  ( findCompletionsWithType
+  , Candidate(..)
+  ) where
 
 #if __GLASGOW_HASKELL__ >= 800
-import Module
+import           Module           (mkModuleNameFS)
 #endif
 
 #if __GLASGOW_HASKELL__ < 710
-import           Data.Foldable (foldMap)
+import           Data.Foldable    (foldMap)
 #endif
 import           Data.List
-import           Data.Map (Map)
-import qualified Data.Map as M
+import           Data.Map         (Map)
+import qualified Data.Map         as M
 import           Data.Maybe
 import           DynFlags
-
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Maybe
+import           ConLike
+import           Data.Monoid      (First (..))
 import           GHC
-import           GhcMonad
-import           GhciInfo (showppr)
+import           GhciInfo         (showppr)
 import           GhciTypes
-import           Name
-import           SrcLoc
+import           GhcMonad
 import           System.Directory
 import           Var
-import ConLike
-type TypedCandidate = (String,Maybe String)
+
+data Candidate = MkCandidate
+  { cId     :: String
+  , cType   :: Maybe String
+  , cModule :: Maybe String
+  } deriving (Show,Eq)
 
 mkTyThingNameType :: TyThing -> (Name,Maybe Type)
 mkTyThingNameType x = case x of
@@ -35,13 +44,13 @@ mkTyThingNameType x = case x of
   AConLike (RealDataCon dc) -> (getName dc,Just $ dataConType dc)
   AConLike (PatSynCon psc) -> (getName psc,Nothing)
   ATyCon tc -> (getName tc, Just $ tyConKind tc)
-  ACoAxiom _ca -> (getName _ca, Nothing)
+  ACoAxiom _coax -> (getName _coax, Nothing)
 
 findCompletionsWithType :: (GhcMonad m)
                 => Map ModuleName ModInfo
                 -> FilePath
                 -> String
-                -> m (Either String [TypedCandidate])
+                -> m (Either String [Candidate])
 findCompletionsWithType infos fp sample = do
   mname <- guessModule infos fp
   case mname of
@@ -49,24 +58,26 @@ findCompletionsWithType infos fp sample = do
     Just name -> case M.lookup name infos of
       Nothing -> return (Left "No module info for the current file! Try loading it?")
       Just moduleInf -> do
-        globalNames <- findGlobalCompletionsWithType sample moduleInf
-        localNames  <- findLocalizedCompletionsWithType (modinfoSpans moduleInf) sample
+        globalNames <- findGlobalCandidates sample moduleInf
+        localNames  <- findLocalCandidates (modinfoSpans moduleInf) sample
         return (Right (take 50 (nub (localNames ++ globalNames))))
 
-findGlobalCompletionsWithType :: GhcMonad m
+findGlobalCandidates :: GhcMonad m
   => String
   -> ModInfo
-  -> m [TypedCandidate]
-findGlobalCompletionsWithType sample moduleInf = do
+  -> m [Candidate]
+findGlobalCandidates sample moduleInf = do
   df <- getDynFlags
   (qual, ident, minfos) <- splitQualIdentInfo
   let
     toplevelNames = concat (mapMaybe modInfoTopLevelScope minfos)
     filteredToplevels = filter (isPrefixOf ident . showppr df)
                         toplevelNames
-  globalNames <- catMaybes <$> mapM (getInfo True) (take 50 filteredToplevels)
+  globalNames <- catMaybes <$> mapM (getInfo True) filteredToplevels
   let globalNameTypes = map (\(t,_r,_,_) -> let (n,tt) = mkTyThingNameType t
-                                            in (qual ++ showppr df n,fmap (showppr df) tt))
+                                                n' = showppr df n
+                                                tt' = fmap (showppr df) tt
+                                            in MkCandidate n' tt' (Just qual))
                      globalNames
   return globalNameTypes
   where
@@ -76,7 +87,7 @@ findGlobalCompletionsWithType sample moduleInf = do
               (map unLoc (modinfoImports moduleInf))
               sample
     splitQualIdentInfo
-      | '.' `elem` sample 
+      | '.' `elem` sample
       , Just (qual, ident, qualModNames) <- qualSrc = do
           minfos <- catMaybes <$> mapM getModInfo qualModNames
           if null minfos
@@ -86,40 +97,40 @@ findGlobalCompletionsWithType sample moduleInf = do
 
 -- | Guess a module name from a file path.
 guessModule :: GhcMonad m
-            => Map ModuleName ModInfo -> FilePath -> m (Maybe ModuleName)
-guessModule infos fp =
-  do target <- guessTarget fp Nothing
-     case targetId target of
-       TargetModule mn -> return (Just mn)
-       TargetFile fp' _ ->
-         case find ((Just fp' ==) .
-                    ml_hs_file . ms_location . modinfoSummary . snd)
-                   (M.toList infos) of
-           Just (mn,_) -> return (Just mn)
-           Nothing ->
-             do fp'' <- liftIO (makeRelativeToCurrentDirectory fp')
-                target' <- guessTarget fp'' Nothing
-                case targetId target' of
-                  TargetModule mn ->
-                    return (Just mn)
-                  _ ->
-                    case find ((Just fp'' ==) .
-                               ml_hs_file . ms_location . modinfoSummary . snd)
-                              (M.toList infos) of
-                      Just (mn,_) ->
-                        return (Just mn)
-                      Nothing -> return Nothing
+            => Map ModuleName ModInfo -> FilePath -> MaybeT m ModuleName
+guessModule infos fp = do
+    target <- lift $ guessTarget fp Nothing
+    case targetId target of
+        TargetModule mn  -> return mn
+        TargetFile fp' _ -> guessModule' fp'
+  where
+    guessModule' :: GhcMonad m => FilePath -> MaybeT m ModuleName
+    guessModule' fp' = case findModByFp fp' of
+        Just mn -> return mn
+        Nothing -> do
+            fp'' <- liftIO (makeRelativeToCurrentDirectory fp')
+
+            target' <- lift $ guessTarget fp'' Nothing
+            case targetId target' of
+                TargetModule mn -> return mn
+                _               -> MaybeT . pure $ findModByFp fp''
+
+    findModByFp :: FilePath -> Maybe ModuleName
+    findModByFp fp' = fst <$> find ((Just fp' ==) . mifp) (M.toList infos)
+      where
+        mifp :: (ModuleName, ModInfo) -> Maybe FilePath
+        mifp = ml_hs_file . ms_location . modinfoSummary . snd
 
 findQualifiedSource :: [ImportDecl n] -> String
                     -> Maybe (String, String, [ModuleName])
-findQualifiedSource importDecls sample =
-  do (ident,qual) <- breakQual sample
-     mnames <- (\nms -> if null nms then Nothing else Just nms)
-                 (foldMap (maybeToList . knownAs qual) importDecls)
-     return (qual++".", ident, mnames)
+findQualifiedSource importDecls sample = do
+  (ident,qual) <- breakQual sample
+  mnames <- (\nms -> if null nms then Nothing else Just nms)
+            (foldMap (maybeToList . knownAs qual) importDecls)
+  return (qual++".", ident, mnames)
   where breakQual xs = case break (== '.') (reverse xs) of
                          (h,_:t) -> Just (reverse h, reverse t)
-                         _ -> Nothing
+                         _       -> Nothing
         knownAs qual m
           | qual == moduleNameString name || maybe False (qual ==) (asName m) =
             Just name
@@ -129,20 +140,20 @@ findQualifiedSource importDecls sample =
 
 -- | Find completions within the local scope of a definition of a
 -- module.
-findLocalizedCompletionsWithType
+findLocalCandidates
   :: GhcMonad m
   => [SpanInfo]
   -> String
-  -> m [TypedCandidate]
-findLocalizedCompletionsWithType spans' prefix =
-  do df <- getDynFlags
-     return (mapMaybe (complete df) spans')
-  where complete
-          :: DynFlags -> SpanInfo -> Maybe TypedCandidate
-        complete df si = do
-          var <- spaninfoVar si
-          let idStr = showppr df var
-              tyStr = showppr df <$> spaninfoType si
-          if isPrefixOf prefix idStr
-            then Just (idStr, tyStr)
-            else Nothing
+  -> m [Candidate]
+findLocalCandidates spans' prefix = do
+  df <- getDynFlags
+  return (mapMaybe (complete df) spans')
+  where
+    complete :: DynFlags -> SpanInfo -> Maybe Candidate
+    complete df si = do
+      var <- spaninfoVar si
+      let idStr = showppr df var
+          tyStr = showppr df <$> spaninfoType si
+      if isPrefixOf prefix idStr
+        then Just $! MkCandidate idStr tyStr Nothing
+        else Nothing
